d0a64f35c7801ce529b6d6a18176f197a8fbe547
thedoft <igor.shishlov@maximumtest.ru>
2023-09-07 13:23:48 +0300

    femove check

--- scripts/check-version.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const semver = require('semver')
-
-const version = '18.16.0'
-if (!semver.satisfies(process.version, version)) {
-  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`)
-  process.exit(1)
-}
-console.log('node version OK - ', process.version)
